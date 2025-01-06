d0dea20f501d6a44c213b38e6d88b3e34c198097
Ramakrishnaragi <152708470+Ramakrishnaragi@users.noreply.github.com>
2025-01-06 15:07:13 +0530

    Update backout.tf

--- day-4-statelock/backout.tf
+++ day-4-statelock/backout.tf
@@ -8,5 +8,5 @@ backend "s3" {
     key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
     dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-bckend resources then day-5-backend config
     encrypt        = true  # Ensures the state is encrypted at rest in S3.
-}
+  }
 }
