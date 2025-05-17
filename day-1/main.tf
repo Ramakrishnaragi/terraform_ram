d5e2a32e831a3168afd5b8f03993ee885e7a9c78
Ramakrishnaragi <152708470+Ramakrishnaragi@users.noreply.github.com>
2025-05-17 21:08:54 +0530

    Update main.tf

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
   ami           = "ami-0fd05997b4dff7aac"
   instance_type = "t2.nano"
-  key_name      = "mumbai"
+  key_name      = "com4"
 tags ={
   Name= "ram-6"
   }
